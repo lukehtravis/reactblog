0340da6ca45cc529a2f9d42dc66a26ddd76ce9b6
luke <lukehtravis@gmail.com>
2017-12-20 15:30:06 -0800

    feature: add actual state heirarchy

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,44 +1,14 @@
-const initialCalendarState = {
-  sunday: {
-    breakfast: null,
-    lunch: null,
-    dinner: null,
-  },
-  monday: {
-    breakfast: null,
-    lunch: null,
-    dinner: null,
-  },
-  tuesday: {
-    breakfast: null,
-    lunch: null,
-    dinner: null,
-  },
-  wednesday: {
-    breakfast: null,
-    lunch: null,
-    dinner: null,
-  },
-  thursday: {
-    breakfast: null,
-    lunch: null,
-    dinner: null,
-  },
-  friday: {
-    breakfast: null,
-    lunch: null,
-    dinner: null,
-  },
-  saturday: {
-    breakfast: null,
-    lunch: null,
-    dinner: null,
-  },
-}
+import { combineReducers } from 'redux';
 
-function test (state = initialCalendarState) {
-
+function posts (state = {}) {
   return state;
 }
 
-export default test
+function comments (state = {}) {
+  return state;
+}
+
+export default combineReducers({
+  posts,
+  comments
+})
